a90688d2ca851d624a8f3fb856187d5daa3e1a7f
holo <81706167+redVerstand@users.noreply.github.com>
2021-11-03 04:09:35 +0500

    Start

---
+++ src/main/java/com/github/redverstand/foodmod/FoodMod.java
@@ -0,0 +1,11 @@
+package com.github.redverstand.foodmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FoodMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
